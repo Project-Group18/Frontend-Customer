d9528e464afd7c0863e2d9c3b8815f7f27cae457
Joni <joni.kuukasjarvi00@outlook.com>
2021-11-09 15:51:44 +0200

    Started working on main page.

--- src/App.js
+++ src/App.js
@@ -6,8 +6,32 @@ function App() {
     <div className="App">
       <header className={styles.background}>
 
-    Hello world
-        
+      <div>
+        <img className={styles.image} src='olivertwistLogo1.jpg' alt='picture'/>
+
+
+            <div> 
+              <input type="text" placeholder= "Search for restaurants or dishes"></input> 
+              <button> Search</button>
+            </div> 
+
+            <div>
+              <i class="fas fa-map-marker-alt"></i>
+                <select id="location">
+                  <option value="oulu">Oulu</option>
+                  <option value="oulu">Oulu</option>
+                  <option value="oulu">Oulu</option>
+                </select>
+                
+            </div>
+
+            <div class="Account">
+              <button>Log in</button>
+              <button>Sign up</button>
+              <i class="fas fa-shopping-cart"></i>
+            </div>
+
+        </div>
       </header>
     </div>
   );
