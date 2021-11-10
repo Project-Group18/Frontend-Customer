846c9fce56dcb6c46da1f5b39b73a581e01a3c29
Joni <joni.kuukasjarvi00@outlook.com>
2021-11-10 14:15:48 +0200

    static frontpage added

--- src/App.js
+++ src/App.js
@@ -1,40 +1,14 @@
 import React from 'react'
-import styles from './App.module.css';
+import Frontpage from './components/Frontpage';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className={styles.background}>
+    <div>
+      <Frontpage/>
 
-      <div>
-        <img className={styles.image} src='olivertwistLogo1.jpg' alt='picture'/>
-
-
-            <div> 
-              <input type="text" placeholder= "Search for restaurants or dishes"></input> 
-              <button> Search</button>
-            </div> 
-
-            <div>
-              <i class="fas fa-map-marker-alt"></i>
-                <select id="location">
-                  <option value="oulu">Oulu</option>
-                  <option value="oulu">Oulu</option>
-                  <option value="oulu">Oulu</option>
-                </select>
-                
-            </div>
-
-            <div class="Account">
-              <button>Log in</button>
-              <button>Sign up</button>
-              <i class="fas fa-shopping-cart"></i>
-            </div>
-
-        </div>
-      </header>
     </div>
   );
-}
 
+  }
 export default App;
