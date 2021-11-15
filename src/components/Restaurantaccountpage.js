aa41e5e453b4ba22cb9ee7f68805f1990bb93009
bobby_nodu@hotmail.com <bobby_nodu@hotmail.com>
2021-11-15 14:19:19 +0200

    Removed header and footer from Customer frontpage

--- src/components/Restaurantaccountpage.js
+++ src/components/Restaurantaccountpage.js
@@ -34,14 +34,66 @@ export default function Restaurantaccountpage() {
                                 <option>Mango</option>
                             </select>
                         </label>
-                        <input type="submit" value="Submit" />
+                        <input type="submit" value="Add" />
                     </form>
                     </p>
                     </div>
                 <div>Edit Profile</div>
             </div>
             <div>Manage Restaurant</div>
-            <div></div>
+            <div>
+                    <div>
+                        <div>Restaurants Name</div>
+                        <input className={styles.inputtextbar} type="searchtext" placeholder= "Enter restaurant's name"></input>
+                        <div>
+                            <p>
+                                <label>
+                                    Restaurant Type:
+                                    <select>
+                                        <option>Fast Food</option>
+                                        <option>Fine Dining</option>
+                                        <option>Casual Dining</option>
+                                        <option>Casual Dining</option>
+                                        <option>Chinese</option>
+                                    </select>
+                                </label>
+                            </p>
+                        </div>
+                        <div>
+                            <div>Opening Hours</div>
+                            <input className={styles.inputtextbar} type="inputtext" placeholder= "input opening hours"></input>
+                        </div>
+                        <div>
+                            <div>Manager's name and phone number</div>
+                                <input className={styles.inputtextbar} type="inputtext" placeholder= "Name and number" required></input>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <p>
+                                <label>
+                                        Price Level:
+                                        <select>
+                                            <option>€</option>
+                                            <option>€€</option>
+                                            <option>€€€</option>
+                                            <option>€€€€</option>
+                                            <option>€€€€€</option>
+                                        </select>
+                                </label>
+                            </p>
+                        </div>
+                        <div>
+                            <div>Address</div>
+                            <input type="text" id="address" placeholder="Type in restaurant address"></input>
+                        </div>
+                    
+                    </div>
+                <div>
+                    Image of Restaurant or logo.
+                </div>
+                        
+            </div>
            
   
           </div>
