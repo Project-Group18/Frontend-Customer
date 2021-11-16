3804ed0161125304b6da345d54fd21abe8f348b7
Elli <elli.pirnes@gmail.com>
2021-11-16 10:55:07 +0200

    tehty managerorderpage ja erilliset header-komponentit

---
+++ src/components/SearchbarLocation.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import styles from './Header.module.css'
+import { useState } from 'react';
+import LoginPopUp from './Login';
+import RegisterPopUp from './Register.js';
+
+export default function SearchbarLocation() {
+    
+    const [buttonLogin, setButtonLogin] = useState(false);
+    const [buttonRegister, setButtonRegister] = useState(false);
+    return (
+        <div>
+            <div className={styles.search}> 
+              <input className={styles.searchbar} type="searchtext" placeholder= "Search for restaurants or dishes"></input> 
+              <button> Search</button>
+            </div> 
+
+            <div className={styles.location}> 
+              <i class="fas fa-map-marker-alt"></i>
+                <select id="location">
+                  <option value="oulu">Oulu</option>
+                  <option value="rovaniemi">Rovaniemi</option>
+                  <option value="turku">Turku</option>
+                </select>
+              
+            </div>
+            <LoginPopUp trigger ={buttonLogin} setTrigger ={setButtonLogin}/>
+            <RegisterPopUp trigger={buttonRegister} setTrigger ={setButtonRegister}/>
+        </div>
+    )
+}
