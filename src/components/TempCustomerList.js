0f39e33ca1561dfcf38b5e551a80bd6056beeeb0
SamuliM <t9musa00@students.oamk.ficode .git config --global user.email t9musa00@students.oamk.fi>
2021-11-25 18:13:06 +0200

    added customerlist+passed data to cust.myaccpage

---
+++ src/components/TempCustomerList.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+
+function TempCustomerList(props) {
+
+    const navigate = useNavigate();
+    const toCustomerAccPage=(props)=>{
+      navigate('customermyaccountpage',
+{state:{id:props.customer_id,name:props.customer_name, email: props.customer_email, address: props.home_address, credit_card: props.credit_card, password: props.customer_password}})        
+    };
+
+
+    return (
+        <div>
+            <br/>
+            {props.customers.map(customer =>
+      <div>
+          <div> <button onClick={()=>{toCustomerAccPage(customer)}}> Customer {customer.customer_id} page</button></div>
+                <br/>
+                <div>Choose customer {customer.customer_id}</div>
+                <ul>ID: {customer.customer_id}</ul>
+                <ul>Name: {customer.customer_name}</ul>
+                <ul>Email: {customer.customer_email}</ul>
+                <ul>Address: {customer.home_address}</ul>
+                <ul>Credit card: {customer.credit_card}</ul>
+                <ul>password: {customer.customer_password}</ul>
+                <br/>
+      </div>
+       )} 
+        </div>
+    )
+}
+
+export default TempCustomerList
