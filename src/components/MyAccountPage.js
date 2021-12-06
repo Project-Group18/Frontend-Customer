a787c00d2847364a97345e09f1eba702ab0255db
SamuliM <t9musa00@students.oamk.ficode .git config --global user.email t9musa00@students.oamk.fi>
2021-12-06 10:35:37 +0200

    customer my account page updated

--- src/components/MyAccountPage.js
+++ src/components/MyAccountPage.js
@@ -1,14 +1,57 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import styles from './CustomerMyAccountPage.module.css';
 import CustomerOrder from './CustomerOrder';
+import api from '../api/config_customer';
+import jwtFromWeb from 'jsonwebtoken';
+import apiCustomer from '../api/config_customer';
 
 function MyAccountPage(props) {
 
+    const [customerInfo, setCustomerInfo] = useState([]);
+    const [customerOrders, setCustomerOrders] = useState([]);
+    const {orders, jwt} = props;
+    const decodedToken = jwtFromWeb.decode(jwt);
+    //load customer data with customer id
+    useEffect(() => {
+        const loadCustomerDataWithJwt =  async () => {
+        try {const res = await api.get(decodedToken.user.id.toString(),
+        {
+            headers: {
+                'Authorization': 'Bearer ' +props.jwt
+            }
+        }
+        );
+        console.log("customer data:")
+        console.log(res);
+        setCustomerInfo(res.data)
+        } catch (err) {//Not in 200 response range
+            console.log(err);
+        }}
+        loadCustomerDataWithJwt();
+    }, [])
 
     //put request here which changes the user information and
 
-    //get request here which gets all orders by customer ID
 
+    //get request here which gets all orders by customer ID
+    //get all orders using customer id
+    useEffect(() => {
+    const loadOrdersWithJWT =  async () => {
+    try {const res = await apiCustomer.get('getOrders/customer/'+decodedToken.user.id.toString(),
+    {
+    headers: {
+    'Authorization': 'Bearer ' +jwt
+    }
+    }
+    );
+    console.log("order data:")
+    console.log(res);
+    setCustomerOrders(res.data)
+    } catch (err) {//Not in 200 response range
+    console.log(err);
+    }}
+    loadOrdersWithJWT();
+  }, [])
 
 
     //old data. Can this be removed?
@@ -42,20 +85,22 @@ function MyAccountPage(props) {
         
         <div class={styles.heading}><b>My Profile</b></div>
             <div class={styles.myaccountinfo}>
-            <div>Name:</div><input className={styles.searchbar} type="name" placeholder="name"  /* onChange={getName} onChange={name} */></input> 
-            <div>Default Address:</div><input className={styles.searchbar} type="address" placeholder= "address" /* onChange={getAddress} onChange={address} */></input> 
-            <div>Credit card number:</div><input className={styles.searchbar} type="creditcard" placeholder= "cc" /* onChange={getCC} onChange={cc} */></input>
+                {customerInfo.map(c => 
+                <tr>
+                    <div>Name:</div><input className={styles.searchbar} type="name" placeholder={c.customer_name}  ></input> 
+                    <div>Default Address:</div><input className={styles.searchbar} type="address" placeholder= {c.home_address} ></input> 
+                    <div>Credit card number:</div><input className={styles.searchbar} type="creditcard" placeholder= {c.credit_card} ></input>
+                </tr>)}
+            
+            
             </div>
-            <button /* onClick={() => setPrint(true)} */class={styles.heading}>Save Changes</button>
+            <button class={styles.heading}>Save Changes</button>
         <div class={styles.heading}><b>Order History</b></div>
    
             <div> 
                 <div className={styles.OrderInfoContainer}>
                 <div className={styles.scrolldiv}>
-                    {
-                        props.orders.map(order => <CustomerOrder {
-                        ...order} key = {order.order_id} />)
-                    }
+                        <CustomerOrder orders={customerOrders} />
                 </div>
             </div> 
 
