9bbc31da112662c5d6bac17406af392e11d8fd2f
SamuliM <t9musa00@students.oamk.ficode .git config --global user.email t9musa00@students.oamk.fi>
2021-12-09 14:06:01 +0200

    moved delivertwist pic to cloudinary

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import styles from './Header.module.css'
 import { useState } from 'react';
+import {Image} from 'cloudinary-react'
 
 /* import RegisterPopUp from './RegisterPopUp.js'; */
-
 
 
 export default function Header(props) {
@@ -11,48 +11,30 @@ export default function Header(props) {
     const {countCartItems} = props;
     console.log("countCartItems")
     console.log(countCartItems)
-    const [buttonShoppingCart, setButtonShoppingCart] = useState(false);
-    const [buttonLogin, setButtonLogin] = useState(false);
-/*     const [buttonRegister, setButtonRegister] = useState(false); */
+
     return (
             <div>
                 <div className={styles.Headercontainer}>
-                    <a href='/' ><img className={styles.image} src='olivertwistLogo1.jpg'/></a>
+                <Image href='/' className={styles.image} cloudName="dczwvybll" publicId="https://res.cloudinary.com/dczwvybll/image/upload/v1639051046/uploadPreset2021/olivertwistLogo1_d645nb.jpg"/>
                     <div className={styles.Account}>
-                        {/* These links were changed from buttons to hrefs because I couldn't redirect from a popup window */}
-                        {/*  Error: useNavigate() may be used only in the context of a <Router> component. */}
                         <div>
                             {props.userLoggedIn ? 
                             <>
                                 {/* If user is logged in, render these buttons */}
                                 <button > <a href='/accountpage' > My account</a></button>
                                 <button onClick={props.logout} ><a href='/'> Log out</a></button>
-                                {/* <a href='/shoppingcartpage'>
-                                    <i className={styles.shoppingcart}className="fas fa-shopping-cart">
-                                        Cart {' '}
-                                        {countCartItems ? (
-                                            <button>{countCartItems}</button>
-                                        ): (
-                                            ''
-                                        )}
-                                    </i>
-                                </a> */}
+                          
                             </>
                             :
                             <>
                                 {/* If user is not logged in, render these buttons */}
                                 <button > <a href='/loginpage' > Log in</a></button>
-                                {/* <button onClick={() => setButtonLogin(true)}>Log in</button> */}
                                 <button > <a href='/registerpage' > Sign up</a></button>
                             </>
                             }
                         </div>
                     </div>
                 </div>
-                {/* Check later if these can be implemented along with the popup windows */}
-                {/* <LoginPopUp trigger ={buttonLogin} setTrigger ={setButtonLogin}/> */}
-                {/*  <RegisterPopUp trigger={buttonRegister} setTrigger ={setButtonRegister}/> */}
-                {/* <ShoppingCartPopUp trigger={buttonShoppingCart} setTrigger ={setButtonShoppingCart}/> */}
             </div>
     )
 }
